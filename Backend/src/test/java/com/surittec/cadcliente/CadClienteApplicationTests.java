7521c3ec6319cbaf8d9edc2c5a85ac58079428e6
Edenio <edenio_cdi@yahoo.com.br>
2021-12-03 18:43:26 -0300

    Criação do ambiente

--- Backend/src/test/java/com/surittec/cadcliente/CadClienteApplicationTests.java
+++ Backend/src/test/java/com/surittec/cadcliente/CadClienteApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.dsvendas;
+package com.surittec.cadcliente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsvendasApplicationTests {
+class CadClienteApplicationTests {
 
 	@Test
 	void contextLoads() {
